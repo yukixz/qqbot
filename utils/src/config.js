ab90aca8022339e32e59843ac72560f86d55ea0b
Dazzy Ding <h@dazzyd.org>
2019-03-18 22:59:15 +0800

    Remove sensitive data

--- utils/src/config.js
+++ utils/src/config.js
@@ -2,14 +2,10 @@ export const CQHTTP_WS_HOST = '127.0.0.1'
 export const CQHTTP_WS_PORT = 12452
 
 export const Stewards = [
-  ***REMOVED***,
 ]
 export const IgnoreUsers = [
-  ***REMOVED***, // AlphaPoi(BOT)
 ]
 export const IgnoreGroups = [
 ]
 export const PoiGroups = [
-  ***REMOVED***,  // poi
-  ***REMOVED***,  // yuki
 ]
