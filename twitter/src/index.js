04bb986185b1bdcaee8da523ee43c5fc6577fc91
Dazzy Ding <h@dazzyd.org>
2019-03-28 18:39:10 +0800

    [twitter] fix bigint

--- twitter/src/index.js
+++ twitter/src/index.js
@@ -3,6 +3,7 @@ import request from 'request-promise-native'
 import { CQWebSocket } from 'cq-websocket'
 import { CQHTTP_WS_HOST, CQHTTP_WS_PORT } from '@qqbot/utils'
 import { CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_TOKEN, Notifies } from './config'
+/* global BigInt */  // HACK: stage-3 built-in object
 
 const QQ = new CQWebSocket({
   host: CQHTTP_WS_HOST,
@@ -11,7 +12,7 @@ const QQ = new CQWebSocket({
 })
 QQ.on('ready', () => log(`QQBot ready`)).connect()
 
-let last_id = 0
+let last_id = BigInt(0)
 const oauth = {
   consumer_key   : CONSUMER_KEY,
   consumer_secret: CONSUMER_SECRET,
@@ -23,12 +24,15 @@ function log(...args) {
   console.log((new Date()).toISOString(), ...args)
 }
 
-async function fetch(since=1) {
+async function fetch(since_id=1) {
+  // eslint-disable-next-line valid-typeof
+  if (typeof since_id === 'bigint')
+    since_id = since_id.toString()
   const resp = await request.get({
     url: 'https://api.twitter.com/1.1/statuses/home_timeline.json',
     qs: {
       count   : 200,
-      since_id: since,
+      since_id: since_id,
       tweet_mode: 'extended',
     },
     oauth: oauth,
@@ -50,17 +54,19 @@ async function init() {
   // }
 
   const timeline = await fetch()
-  last_id = timeline[0].id + 1
+  last_id = BigInt(timeline[0].id_str)
   log(`Last timeline id: ${last_id}`)
 }
 
 async function broadcast() {
-  log(`Broadcast since ${last_id}`)
-  const timeline = await fetch(last_id)
-  last_id = timeline[0].id + 1
-
+  log(`Broadcast since id: ${last_id}`)
+  const timeline = await fetch(last_id + 1n)
   for (const tweet of timeline.reverse()) {
-    log(`Broadcasting ${tweet.id} @${tweet.user.screen_name}`)
+    log(`Broadcasting ${tweet.id_str} @${tweet.user.screen_name}`)
+    const tweet_id = BigInt(tweet.id_str)
+    if (tweet_id > last_id)
+      last_id = tweet_id
+
     let { name, screen_name } = tweet.user
     let _date = moment(tweet.created_at, "ddd MMM DD HH:mm:ss Z YYYY").tz('Asia/Tokyo')
     let date  = `${_date.format("YYYY-MM-DD HH:mm:ss")} JST`
@@ -78,8 +84,8 @@ async function broadcast() {
   }
 }
 
-;(async () => {
+(async () => {
   await init()
   setInterval(broadcast, 60 * 1000)
-  // setTimeout(broadcast, 1000)
+  // setTimeout(broadcast, 0)
 })()
